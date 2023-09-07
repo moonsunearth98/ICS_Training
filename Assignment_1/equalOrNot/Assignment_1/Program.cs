7c1bd9c2e9c842716ec321be8051cb1e65e5399c
Prachi Baranwal <prachib@infinite.com>
2023-09-07 08:58:55 -0700

    My First Git Program

---
+++ Assignment_1/equalOrNot/Assignment_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input 1st number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Input 2nd number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 == num2)
+        {
+            Console.WriteLine($"{num1} and {num2} are equal");
+        }
+        else
+        {
+            Console.WriteLine($"{num1} and {num2} are not equal");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Assignment_2/Average_Value/Average_Value/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[5]; 
+        int sum = 0;
+        int min = int.MaxValue;
+        int max = int.MinValue;
+
+      
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Enter value {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+
+            sum += numbers[i];
+            if (numbers[i] < min)
+                min = numbers[i];
+            if (numbers[i] > max)
+                max = numbers[i];
+        }
+
+        double average = (double)sum / numbers.Length;
+
+        Console.WriteLine($"Average value: {average}");
+        Console.WriteLine($"Minimum value: {min}");
+        Console.WriteLine($"Maximum value: {max}");
+        Console.ReadLine();
+    }
+}
