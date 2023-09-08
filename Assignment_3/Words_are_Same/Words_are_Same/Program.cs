42562b42ab30a96dd2d48e7be1db023a6f5544d7
Prachi Baranwal <prachib@infinite.com>
2023-09-07 23:10:27 -0700

    My second folder

---
+++ Assignment_3/Words_are_Same/Words_are_Same/Program.cs
@@ -0,0 +1,31 @@
+﻿//Write a program in C# to accept two words from user and find out if they are same. 
+
+using System;
+
+
+
+class Program
+
+{
+   static void Main()
+
+    {
+        Console.WriteLine("Enter the first string:");
+        string input1 = Console.ReadLine();
+        Console.WriteLine("Enter the second string:");
+        string input2 = Console.ReadLine();
+        if (input1 == input2)
+        {
+            Console.WriteLine("Both strings are the same.");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("The strings are not the same.");
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
---
+++ Assignment_3/length_of_string/length_of_string/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a program in C# to accept a word from the user and display the length of it.
+
+
+using System;
+
+
+
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        Console.Write("Enter a word: ");
+        string word = Console.ReadLine();
+        int length = word.Length;
+        Console.WriteLine("The length of the word '{0}' is {1} characters.", word, length);
+        Console.ReadLine();
+
+    }
+
+}
