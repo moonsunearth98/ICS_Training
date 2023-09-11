b1e135a1138efdc3bf194b95969aa08b5bda3727
Prachi Baranwal <prachib@infinite.com>
2023-09-10 22:12:55 -0700

    Add code_base folder

---
+++ CodeBase_Test_C#/Multiplication_Table/Multiplication_Table/Program.cs
@@ -0,0 +1,19 @@
+﻿//Que2:- Write a C# Sharp program that prints the multiplication table of a number as input.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Multiplication Table for {number}:");
+        for (int i = 0; i <= 10; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} * {i} = {result}");
+            Console.ReadLine();
+        }
+    }
+}
