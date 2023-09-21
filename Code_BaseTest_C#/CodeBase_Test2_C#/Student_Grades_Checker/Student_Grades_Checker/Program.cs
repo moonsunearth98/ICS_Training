24e0ff50130c787ad655e5878410e3a61415dc2f
Prachi Baranwal <prachib@infinite.com>
2023-09-21 22:29:25 +0530

    My C# Task

--- Code_BaseTest_C#/CodeBase_Test2_C#/Student_Grades_Checker/Student_Grades_Checker/Program.cs
+++ Code_BaseTest_C#/CodeBase_Test2_C#/Student_Grades_Checker/Student_Grades_Checker/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/**[09:45] Banurekha (Guest)
+
+1. Create an Abstract class Student with  Name, StudentId, Grade as members and also an abstract method Boolean Ispassed(grade) which takes grade as an input and checks whether student passed the course or not.  
+Create 2 Sub classes Undergraduate and Graduate that inherits all members of the student and overrides Ispassed(grade) method
+For the UnderGrad class, if the grade is above 70.0, then isPassed returns true, otherwise it returns false. For the Grad class, if the grade is above 80.0, then isPassed returns true, otherwise returns false.
+Test the above by creating appropriate objects**/
+
+
+
+
+using System;
 
 
 public abstract class Student
