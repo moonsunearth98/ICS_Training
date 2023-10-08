baa10e617f534bb3f530ee2d49e08d607b3a791c
Prachi Baranwal <prachib@infinite.com>
2023-10-08 11:32:23 +0530

    My Assignment

---
+++ CSharp_Training/Assignments/Assignment_1/A1/positive_negative.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class positive_negative
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine($"{number} is a positive number");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine($"{number} is a negative number");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero");
+        }
+        Console.ReadLine();
+    }
+}
