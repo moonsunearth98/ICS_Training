1bbf58a24eba6932a9d91d96596c6f9c5cf58da1
Prachi Baranwal <prachib@infinite.com>
2023-11-11 14:15:42 +0530

    My Git Task

---
+++ CodeBase_Test8_MVC/Question_1/Question_1/Controllers/CodeController.cs
@@ -0,0 +1,39 @@
+﻿using Question_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Question_1.Controllers
+{
+    public class CodeController : Controller
+    {
+        // GET: Code
+        private NorthwindEntities db = new NorthwindEntities(); // Replace with your data context name
+        // GET: Code
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CustomersInGermany()
+        {
+            var germanyCustomers = db.Customers.Where(c => c.Country == "Germany").ToList();
+            return View(germanyCustomers);
+        }
+
+        public ActionResult CustomerDetailsByOrderId(int orderId = 10248)
+        {
+            var customer = db.Customers
+                .Where(c => c.Orders.Any(o => o.OrderID == orderId))
+                .SingleOrDefault();
+
+            if (customer == null)
+            {
+                return HttpNotFound(); // Return a 404 status code if the customer is not found.
+            }
+
+            return View(customer);
+        }
+    }
+}
